40c4f7bf3b4ed8612ce6224ff59d477f49c6eaed
Claudio Ciavarella <claudio.ciavarella@gmail.com>
2025-02-04 00:32:38 +0100

    1 commit
    import iniziale

---
+++ script.js
@@ -0,0 +1,32 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+
+  /* Open */
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+  }
